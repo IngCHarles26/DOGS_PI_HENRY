cd3f52d7a419debd73ee0f072f568b5e95d0386b
CarlosCo_Dev <condorpasa26@gmail.com>
2023-10-26 21:55:39 -0500

    listo creo para la prueba

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -11,6 +11,7 @@ const maxWeight = 157;
 const minWeight = 0.55;
 const minHeight = 3.5;
 const maxHeight = 200;
+const maxYearsLife = 31;
 
 const initialForm = {
   origin:'DB',
@@ -42,9 +43,9 @@ function Form() {
   const addTemp = (e)=>{
     e.preventDefault();
     const {value} = e.target
-    if(!formData.temperament.includes(value)) 
+    if(!formData.temperament.includes(tempToId[value])) {
     setSelectedTemps([...selectedTemps,value])
-    setFormData({...formData,temperament:[...formData.temperament,tempToId[value]]});
+    setFormData({...formData,temperament:[...formData.temperament,tempToId[value]]});}
   }
 
   const handleTemp = (e)=>{
@@ -57,30 +58,42 @@ function Form() {
     let val,aux;
     if(name === 'name'){
       value = value.replaceAll(minMax,' ');
-      val = !allNameDogs.includes(value.toLowerCase()) && Boolean(value);
+      val = !allNameDogs.includes(value.toLowerCase().trim()) && Boolean(value) && value !== ' ';
       setValidateForm({...validateForm,name:val})
     }else {
-      value = value.replaceAll(minMax,'').replaceAll(onlyNumbers,'');
+      value = value.replaceAll(minMax,'').replaceAll(onlyNumbers,'')
       aux = value.split('-').sort((a,b)=> +a - +b);
-      val = aux.length<=2 && Boolean(value) && 
-            aux.every(el=>typeof +el === 'number' && el !== '') &&
-            (name == 'height');
+      if(name === 'height'){
+        val = aux.every(el=>el && el>minHeight && el<maxHeight);
+      }else if(name === 'weight'){
+        val = aux.every(el=>el && el>minWeight && el<maxWeight);
+      }else if(name === 'yearsLife'){
+        val = aux.every(el=>el && el<maxYearsLife)
+      }
       setValidateForm({...validateForm,[name]:val})
     }
-    // console.log(formData);
+    console.log(validateForm);
     setFormData({
-      ...formData,
-      [name]:value,
-    })
+        ...formData,
+        [name]:value,
+      })
   }
   
   const handleSubmit = (e)=>{
     e.preventDefault();
     console.log(validateForm)
     if(Object.values(validateForm).every(el=>el)){
+      let {name, height, weight, yearsLife, temperament} = formData;
+      setFormData({...formData,
+        name: name.trim(),
+        height: height.split('-').sort((a,b)=> +a - +b).join('-'),
+        weight: weight.split('-').sort((a,b)=> +a - +b).join('-'),
+        yearsLife: yearsLife.split('-').sort((a,b)=> +a - +b).join('-'),
+        temperament: temperament.length ? temperament : [1]
+      })
       console.log(formData)
       axios.post(URL,formData)
-        .then(res=>{window.alert('guardado con éxito');setFormData(initialForm);navigate('/')})
+        .then(res=>{window.alert('guardado con éxito');setFormData(initialForm);navigate('/home')})
         .catch(err=>console.log(err.message))
     }else{
       window.alert('Valores no permitidos')
@@ -91,10 +104,10 @@ function Form() {
     <div className="formMax">
       <form className="formComponent" onSubmit={handleSubmit}>
         <h1 className="formTitle">Create your Friend</h1>
-        <input className="formInput" onChange={handleInput} value={formData.name} name="name" type="text" placeholder="🐶 BREED"/>
-        <input className="formInput" onChange={handleInput} value={formData.height} name="height" type="text" placeholder="📏 HEIGHT  ( min - max ) cm"/>
-        <input className="formInput" onChange={handleInput} value={formData.weight} name="weight" type="text" placeholder="🏋 WEIGHT  ( min - max ) kg"/>
-        <input className="formInput" onChange={handleInput} value={formData.yearsLife} name="yearsLife" type="text" placeholder="🪦 LIFE  ( min - max ) years"/>
+        <input className={"formInput " + (!validateForm.name&&'badInput')} onChange={handleInput} value={formData.name} name="name" type="text" placeholder="🐶 BREED"/>
+        <input className={"formInput " + (!validateForm.height&&'badInput')} onChange={handleInput} value={formData.height} name="height" type="text" placeholder="📏 HEIGHT  ( min - max ) cm"/>
+        <input className={"formInput " + (!validateForm.weight&&'badInput')} onChange={handleInput} value={formData.weight} name="weight" type="text" placeholder="🏋 WEIGHT  ( min - max ) kg"/>
+        <input className={"formInput " + (!validateForm.yearsLife&&'badInput')} onChange={handleInput} value={formData.yearsLife} name="yearsLife" type="text" placeholder="🪦 LIFE  ( min - max ) years"/>
         <input className="formInput" onChange={handleTemp} value={temp} type="search" placeholder="Temperamentos" />
         <div className="formListTemps">
           {temp && allTemp.slice(2)
@@ -104,6 +117,7 @@ function Form() {
                 <button value={el} onClick={addTemp} key={`temp_form_${ix}`}>{el}</button>)}
         </div>
         <div className="formListChoices">
+          <p className="formListChoicesTitle">Selected Temperaments</p>
           {selectedTemps.map((el,ix)=><span key={`tem_to_db_${ix}`}>{el} </span>)}
         </div>
         <button className="formSubmitButton" type="submit">Save</button>
